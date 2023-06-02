9e3aea6b19abd465342fd750e57a3e0739299ea9
dosepehr <dosepehr@gmail.com>
2023-06-03 02:29:48 +0330

    show products in dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
-
-import './App.css'
-
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addProductAction } from './redux/store/Products';
+import './App.css';
+import Products from './Products';
 export default function App() {
-  return (
-    <h1>React-Redux</h1>
-  )
+    const dispatch = useDispatch();
+    const [productName, setProductName] = useState('');
+    const productNameHandler = () => {
+        dispatch(addProductAction(productName));
+    };
+    return (
+        <>
+            <h1>React-Redux</h1>
+            <input
+                type='text'
+                name=''
+                id=''
+                onChange={(e) => setProductName(e.target.value)}
+            />
+            <button onClick={productNameHandler}>add product</button>
+            <Products />
+        </>
+    );
 }
+
