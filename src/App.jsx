1d0924971607bf20a337227d65990e5fdb1fc563
dosepehr <dosepehr@gmail.com>
2023-06-03 03:04:25 +0330

    refactor App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,7 @@ import Products from './Products';
 export default function App() {
     const dispatch = useDispatch();
     const [productName, setProductName] = useState('');
-    const productNameHandler = () => {
-        dispatch(addProductAction(productName));
-    };
+
     return (
         <>
             <h1>React-Redux</h1>
@@ -18,7 +16,9 @@ export default function App() {
                 id=''
                 onChange={(e) => setProductName(e.target.value)}
             />
-            <button onClick={productNameHandler}>add product</button>
+            <button onClick={() => dispatch(addProductAction(productName))}>
+                add product
+            </button>
             <Products />
         </>
     );
