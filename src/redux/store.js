ea49f9295c282103277420248cbc03185faf31bb
dosepehr <dosepehr@gmail.com>
2023-06-03 01:38:16 +0330

    make store using combineReducers

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,15 @@
-import { createStore } from 'redux';
-import reducer from './reducer';
-const store = createStore(reducer);
+import { createStore, combineReducers } from 'redux';
+
+import articlesReducer from './store/Articles';
+import commentReducer from './store/Comments';
+import productsReducer from './store/Products';
+
+const store = createStore(
+    combineReducers({
+        productsReducer,
+        commentReducer,
+        articlesReducer,
+    })
+);
 
 export default store;
