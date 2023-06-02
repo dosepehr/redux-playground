f4a2db964c36936c4e621fa38517b276b084830e
dosepehr <dosepehr@gmail.com>
2023-06-03 03:24:03 +0330

    refactor store using redux-toolkit

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,19 +1,14 @@
-import { createStore, combineReducers } from 'redux';
-import { composeWithDevTools } from '@redux-devtools/extension';
-import logger from 'redux-logger';
-import thunk from 'redux-thunk';
-import { applyMiddleware } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
 
 import productsReducer from './store/Products';
 import articlesReducer from './store/Articles';
 import commentReducer from './store/Comments';
-const store = createStore(
-    combineReducers({
+const store = configureStore({
+    reducer: {
         products: productsReducer,
         articles: articlesReducer,
         comments: commentReducer,
-    }),
-    composeWithDevTools(applyMiddleware(thunk, logger))
-);
+    },
+});
 
 export default store;
