b0a4abacd3e1507bcf21dcf06e8ac26be9ef8f79
dosepehr <dosepehr@gmail.com>
2023-06-03 03:04:15 +0330

    apply middlewares

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,15 +1,19 @@
 import { createStore, combineReducers } from 'redux';
+import { composeWithDevTools } from '@redux-devtools/extension';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+import { applyMiddleware } from 'redux';
 
 import productsReducer from './store/Products';
 import articlesReducer from './store/Articles';
 import commentReducer from './store/Comments';
-
 const store = createStore(
     combineReducers({
         products: productsReducer,
         articles: articlesReducer,
         comments: commentReducer,
-    })
+    }),
+    composeWithDevTools(applyMiddleware(thunk, logger))
 );
 
 export default store;
